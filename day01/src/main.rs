d69450f5653096d05f462f3912124e953a1bb058
agga <adrian@nukez.de>
2024-12-03 08:31:11 +0100

    cargo-format + add day03

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,10 +1,8 @@
-
 use std::env;
 use std::fs;
 use std::io;
 
-fn main() -> io::Result<()>{
-
+fn main() -> io::Result<()> {
     let current_dir = env::current_dir()?;
     println!("Current working directory: {}", current_dir.display());
 
@@ -15,7 +13,7 @@ fn main() -> io::Result<()>{
     let mut left = vec![];
     let mut right = vec![];
 
-    for line in contents.lines(){
+    for line in contents.lines() {
         let mut words = line.split_whitespace();
         left.push(words.next().unwrap().parse::<usize>().unwrap());
         right.push(words.next().unwrap().parse::<usize>().unwrap());
@@ -24,21 +22,20 @@ fn main() -> io::Result<()>{
     left.sort();
     right.sort();
 
-    let result_part1: usize = left.iter().zip(right.iter())
-        .map(|(l, r)|l.abs_diff(r.clone()))
+    let result_part1: usize = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| l.abs_diff(r.clone()))
         .sum();
 
     println!("part01 {}", result_part1);
 
     let result_part2: usize = left
-       .iter()
-       .map(|number|{
-        number * right.iter().filter(|r| &number == r).count()
-       })
-       .sum();
+        .iter()
+        .map(|number| number * right.iter().filter(|r| &number == r).count())
+        .sum();
 
     println!("part02 {}", result_part2);
 
-    
     Ok(())
 }
